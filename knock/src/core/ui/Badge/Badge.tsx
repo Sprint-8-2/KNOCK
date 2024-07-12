f1a17da04eb7ea5f0bee8cd7a055d1dd8b9cc28a
OK <ok@OK.local>
2024-07-13 00:46:31 +0900

    Feature 공통 Badge 컴포넌트 추가 #31

---
+++ knock/src/core/ui/Badge/Badge.tsx
@@ -0,0 +1,18 @@
+import styles from'../../styles/badge.module.scss';
+
+interface BadgeProps {
+  status: string;
+}
+
+const Badge: React.FC<BadgeProps> = ({ status = 'answered' }) => {
+  return (
+    <div
+      className={`${styles['Badge']} ${status === 'answered' ? styles['answered'] : styles['unanswered']}`}
+    >
+      {status === 'answered' ? '답변완료' : '미답변'}
+    </div>
+  );
+};
+
+export default Badge;
+
