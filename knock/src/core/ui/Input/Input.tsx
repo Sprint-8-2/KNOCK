43db23b6c5eb370552a5b15b0afebf3b34658c9c
seunghyun <tmdgus7048@naver.com>
2024-07-12 18:44:50 +0900

    feat: 공통 input컴포넌트 추가 #8

---
+++ knock/src/core/ui/Input/Input.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import styles from '../../styles/input.module.scss';
+
+interface InputProps {
+  children: React.ReactNode;
+  value: string;
+  onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const Input = ({ children, value, onKeyDown, onChange }: InputProps) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+  return (
+    <div
+      className={`${styles['form-input']} ${isFocused ? styles['form-input--focused'] : ''}`}
+    >
+      <div className={styles['form-input__icon']}>{children}</div>
+      <input
+        type="text"
+        className={styles['form-input__input']}
+        placeholder="이름을 입력하세요"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        value={value}
+        onKeyDown={onKeyDown}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
