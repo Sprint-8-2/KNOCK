ccf3bd43304cb1937e9f56754c5d1269811c1462
L1m3Kun <sungku2757@gmail.com>
2024-07-13 23:11:07 +0900

    fix: 공통 API 네이밍 변경 #44
    userId -> subjectId

--- knock/src/lib/api/Answers.ts
+++ knock/src/lib/api/Answers.ts
@@ -8,17 +8,17 @@ import { apiHandler } from './APIHandler';
 
 const DOMAIN = 'answer/';
 
-export const getAnswer = async ({ userId, options }: AnswerParams) => {
-  const url = `${DOMAIN}${userId}/`;
+export const getAnswer = async ({ subjectId, options }: AnswerParams) => {
+  const url = `${DOMAIN}${subjectId}/`;
   return apiHandler.get<QuestionDetailResponseAnswer>(url, options);
 };
 
 export const putAnswer = async ({
-  userId,
+  subjectId,
   body,
   options,
 }: AnswerHasBodyParams) => {
-  const url = `${DOMAIN}${userId}/`;
+  const url = `${DOMAIN}${subjectId}/`;
   return apiHandler.put<AnswerBody, QuestionDetailResponseAnswer>(
     url,
     body,
@@ -27,11 +27,11 @@ export const putAnswer = async ({
 };
 
 export const patchAnswer = async ({
-  userId,
+  subjectId,
   body,
   options,
 }: AnswerHasBodyParams) => {
-  const url = `${DOMAIN}${userId}/`;
+  const url = `${DOMAIN}${subjectId}/`;
   return apiHandler.put<AnswerBody, QuestionDetailResponseAnswer>(
     url,
     body,
@@ -39,7 +39,7 @@ export const patchAnswer = async ({
   );
 };
 
-export const deleteAnswer = async ({ userId, options }: AnswerParams) => {
-  const url = `${DOMAIN}${userId}/`;
+export const deleteAnswer = async ({ subjectId, options }: AnswerParams) => {
+  const url = `${DOMAIN}${subjectId}/`;
   return apiHandler.delete<undefined>(url, options);
 };
