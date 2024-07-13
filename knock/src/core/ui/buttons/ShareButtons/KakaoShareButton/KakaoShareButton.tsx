369be2d6792736a3e829e77129af1ffe36fd80f6
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-07-13 14:20:43 +0900

    fix: 공통 공유버튼컴포넌트 prop 수정 #7
    - RoundButton 컴포넌트 className prop 추가, backgrund color prop 삭제
    - 상위 컴포넌트에서 prop 으로 styles 사용하여 스타일 적용 가능

--- knock/src/core/ui/buttons/ShareButtons/KakaoShareButton/KakaoShareButton.tsx
+++ knock/src/core/ui/buttons/ShareButtons/KakaoShareButton/KakaoShareButton.tsx
@@ -1,10 +1,14 @@
 import { ReactComponent as Icon } from '../../../../assets/icon/Kakaotalk.svg';
 import RoundButton from '../../RoundButton/RoundButton';
+import styles from './KakaoShareButton.module.scss';
 
 const KakaoShareButton = () => {
   return (
     <>
-      <RoundButton handleClick={() => {}} backgroundColor="#FEE500">
+      <RoundButton
+        className={styles['button--custom-color']}
+        handleClick={() => {}}
+      >
         <Icon />
       </RoundButton>
     </>
