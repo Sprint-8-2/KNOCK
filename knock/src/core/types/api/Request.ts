816f34fcb6bd82bb4a9ec693a7c3f026b4b79ac0
L1m3Kun <sungku2757@gmail.com>
2024-07-13 22:30:10 +0900

    fix: 공통 API interface 정리 #44

---
+++ knock/src/core/types/api/Request.ts
@@ -0,0 +1,65 @@
+export interface APIOptions {
+  options?: Omit<RequestInit, 'method' | 'body'>;
+}
+
+export interface AnswerParams extends APIOptions {
+  userId: number | string;
+}
+
+export interface AnswerHasBodyParams extends AnswerParams {
+  body: AnswerBody;
+}
+
+export interface AnswerBody {
+  content: string;
+  isRejected: boolean;
+}
+
+export interface QuestionDetailsProps extends APIOptions {
+  questionId: number | string;
+}
+
+export interface QuestionReactionProps extends APIOptions {
+  questionId: number | string;
+  type: 'like' | 'dislike';
+}
+
+export interface QuestionAnswerProps extends APIOptions {
+  questionId: number | string;
+  content: string;
+  isRejected: boolean;
+}
+
+export interface SubjectListParams extends APIOptions {
+  limit?: number;
+  offset?: number;
+}
+
+export interface SubjectGetDetailParams extends APIOptions {
+  userId: number | string;
+}
+
+export interface CreateSubjectProps extends APIOptions {
+  name: string;
+}
+
+export interface DeleteSubjectProps extends APIOptions {
+  subjectId: number | string;
+}
+
+export interface SubjectQuestionListParams extends SubjectListParams {
+  subjectId: number | string;
+}
+
+export interface CreateSubjectQuestionAnswer {
+  content: string;
+  isRejected: boolean;
+}
+
+export interface CreateSubjectQuestionProps extends APIOptions {
+  subjectId: number | string;
+  content: string;
+  like?: number;
+  dislike?: number;
+  answer?: CreateSubjectQuestionAnswer;
+}
--- knock/src/lib/api/Questions.ts
+++ knock/src/lib/api/Questions.ts
@@ -1,15 +1,15 @@
-import { APIOptions } from '../../core/types/api/Request';
 import {
+  QuestionAnswerProps,
+  QuestionDetailsProps,
+  QuestionReactionProps,
+} from '../../core/types/api/Request';
+import {
+  QuestionAnswerResponse,
   QuestionDetailResponse,
-  QuestionReactionResponse,
 } from '../../core/types/api/Response';
 import { apiHandler } from './APIHandler';
 
 const DOMAIN = 'questions/';
-
-interface QuestionDetailsProps extends APIOptions {
-  questionId: number | string;
-}
 
 export const getQuestionDetails = async ({
   questionId,
@@ -18,64 +18,39 @@ export const getQuestionDetails = async ({
   const url = `${DOMAIN}${questionId}/`;
   return await apiHandler.get<QuestionDetailResponse>(url, options);
 };
-
-interface DeleteQuestionProps extends APIOptions {
-  questionId: number | string;
-}
-
 export const deleteQuestion = async ({
   questionId,
   options,
-}: DeleteQuestionProps) => {
+}: QuestionDetailsProps) => {
   const url = `${DOMAIN}${questionId}/`;
   return await apiHandler.delete<undefined>(url, options);
 };
-
-interface CreateQuestionReactionProps extends APIOptions {
-  questionId: number | string;
-  type: 'like' | 'dislike';
-}
 
 export const createQuestionReaction = async ({
   questionId,
   type,
   options,
-}: CreateQuestionReactionProps) => {
+}: QuestionReactionProps) => {
   const url = `${DOMAIN}${questionId}/reaction/`;
   const body = { type };
   return await apiHandler.post<
-    Omit<CreateQuestionReactionProps, 'questionId' | 'options'>,
-    QuestionReactionResponse
+    Omit<QuestionReactionProps, 'questionId' | 'options'>,
+    QuestionDetailResponse
   >(url, body, options);
 };
-
-interface CreateQuestionAnswerProps extends APIOptions {
-  questionId: number | string;
-  content: string;
-  isRejected: boolean;
-}
-
-interface CreateQuestionAnswerResponse {
-  answerId: number;
-  questionId: number;
-  content: string;
-  isRejected: boolean;
-  createdAt: string;
-}
-
 export const createQuestionAnswer = async ({
   questionId,
   content,
   isRejected,
   options,
-}: CreateQuestionAnswerProps) => {
+}: QuestionAnswerProps) => {
   const url = `${DOMAIN}${questionId}/answers/`;
   const body = {
     content,
     isRejected,
   };
   return await apiHandler.post<
-    Omit<CreateQuestionAnswerProps, 'questionId' | 'options'>,
-    CreateQuestionAnswerResponse
+    Omit<QuestionAnswerProps, 'questionId' | 'options'>,
+    QuestionAnswerResponse
   >(url, body, options);
 };
