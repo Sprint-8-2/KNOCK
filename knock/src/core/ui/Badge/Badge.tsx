0a2f2c8c95688d2af5f7201dc4740f91711ba0c2
OK <ok@OK.local>
2024-07-13 11:24:47 +0900

    Fix:공통Badge컴포넌트수정#31

--- knock/src/core/ui/Badge/Badge.tsx
+++ knock/src/core/ui/Badge/Badge.tsx
@@ -1,15 +1,15 @@
 import styles from '../../styles/badge.module.scss';
 
 interface BadgeProps {
-  status: string;
+  status: boolean;
 }
 
 const Badge: React.FC<BadgeProps> = ({ status }) => {
   return (
     <div
-      className={`${styles['Badge']} ${status === 'answered' ? styles['answered'] : styles['unanswered']}`}
+      className={`${styles['Badge']} ${status ? styles['Badge__answered'] : styles['Badge__unanswered']}`}
     >
-      {status === 'answered' ? '답변완료' : '미답변'}
+      {status ? '답변완료' : '미답변'}
     </div>
   );
 };
