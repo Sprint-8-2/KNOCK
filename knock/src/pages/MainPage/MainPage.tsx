3fbed8dbba0c918c202fd05acba4ff199421becb
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-07-15 12:37:29 +0900

    fix: 공통 메인페이지 초기코드로 수정 #2

--- knock/src/pages/MainPage/MainPage.tsx
+++ knock/src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,5 @@
-import BoxButton from '../../core/ui/buttons/BoxButton/BoxButton';
-
 function MainPage() {
-  return (
-    <>
-      <BoxButton handleClick={() => {}} isLightTheme={true}>
-        버튼
-      </BoxButton>
-    </>
-  );
+  return <></>;
 }
 
 export default MainPage;
