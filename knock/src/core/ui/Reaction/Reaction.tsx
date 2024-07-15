1c46c3e57b2593835e8ddce6ca040a5a08f1f1c4
OK <ok@OK-2.local>
2024-07-15 18:07:50 +0900

    Fix:props추가 및 좋아요/싫어요버튼을 하나로,더블클릭시 비활성화

--- knock/src/core/ui/Reaction/Reaction.tsx
+++ knock/src/core/ui/Reaction/Reaction.tsx
@@ -5,51 +5,70 @@ import AfterUnLikeIcon from '../../assets/icon/unlike/After-unLike.svg';
 import styles from '../../styles/Reaction.module.scss';
 import { useState } from 'react';
 
-const Reaction: React.FC<ReactionProps> = ({ likeCount }) => {
-  const [showlikeCount, setLikeCount] = useState(likeCount);
+interface ReactionProps {
+  likeCount: number;
+  onLikeClick: () => void;
+  onLikeDoubleClick: () => void;
+}
+
+const Reaction: React.FC<ReactionProps> = ({
+  likeCount,
+  onLikeClick,
+  onLikeDoubleClick,
+}) => {
   const [liked, setLiked] = useState(false);
   const [unLiked, setUnLiked] = useState(false);
 
   const handleLikeClick = () => {
-    setLikeCount((prev) => prev + 1);
-    setLiked(true);
-    setUnLiked(false);
+    if (!liked) {
+      onLikeClick();
+      setLiked(true);
+      setUnLiked(false);
+    } else {
+      onLikeClick();
+      setLiked(false);
+      setUnLiked(true);
+    }
   };
 
-  const handleUnLikeClick = () => {
-    setUnLiked(!unLiked);
+  const handleLikeDoubleClick = () => {
     setLiked(false);
+    setUnLiked(false);
+    onLikeDoubleClick();
   };
 
   return (
-    <div className={`${styles['container']}`}>
+    <div
+      className={`${styles['container']}`}
+      onDoubleClick={handleLikeDoubleClick}
+    >
       <div className={`${styles['label']}`} onClick={handleLikeClick}>
-        <img src={liked ? AfterLikeIcon : BeforeLikeIcon} alt="좋아요" />
-        <button
-          className={`
-          ${styles['text']}
-          ${liked ? styles['like'] : ''}
-          `}
-        >
-          좋아요 {liked ? showlikeCount : ''}
-        </button>
-      </div>
-      <div className={`${styles['label']}`} onClick={handleUnLikeClick}>
-        <img src={unLiked ? AfterUnLikeIcon : BeforeUnLikeIcon} alt="싫어요" />
-        <button
-          className={`
-          ${styles['text']} 
-          ${unLiked ? styles['hate'] : ''}
-          `}
-        >
-          싫어요
-        </button>
+        <div>
+          <img src={liked ? AfterLikeIcon : BeforeLikeIcon} alt="좋아요" />
+          <button
+            className={`
+            ${styles['text']}
+            ${liked ? styles['like'] : ''}
+            `}
+          >
+            좋아요 {liked ? likeCount : ''}
+          </button>
+          <img
+            src={unLiked ? AfterUnLikeIcon : BeforeUnLikeIcon}
+            alt="싫어요"
+          />
+          <button
+            className={`
+            ${styles['text']} 
+            ${unLiked ? styles['hate'] : ''}
+            `}
+          >
+            싫어요
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-interface ReactionProps {
-  likeCount: number;
-}
 export default Reaction;
