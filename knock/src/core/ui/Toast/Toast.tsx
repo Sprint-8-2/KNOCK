9b04638dcd1163498392a2fe0fdeb6b9f339e49f
OK <ok@OK-2.local>
2024-07-15 16:56:23 +0900

    Fix:useEffect삭제,interface상위로 이동

--- knock/src/core/ui/Toast/Toast.tsx
+++ knock/src/core/ui/Toast/Toast.tsx
@@ -1,33 +1,17 @@
-import React, { useState, useEffect } from 'react';
 import styles from '../../styles/toast.module.scss';
 
-const Toast: React.FC<ToastProps> = ({ showToast, toastMessage }) => {
-  const [showToastState, setShowToast] = useState(true);
+interface ToastProps {
+  toastMessage: string;
+}
 
-  useEffect(() => {
-    if (showToast) {
-      const timer = setTimeout(() => {
-        setShowToast(false);
-      }, 2000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [showToast]);
-
+const Toast: React.FC<ToastProps> = ({ toastMessage }) => {
   return (
     <>
-      {showToastState && (
-        <div className={styles['layout']}>
-          <div className={styles['text']}>{toastMessage}</div>
-        </div>
-      )}
+      <div className={styles['layout']}>
+        <div className={styles['text']}>{toastMessage}</div>
+      </div>
     </>
   );
 };
-
-interface ToastProps {
-  showToast: boolean;
-  toastMessage: string;
-}
 
 export default Toast;
