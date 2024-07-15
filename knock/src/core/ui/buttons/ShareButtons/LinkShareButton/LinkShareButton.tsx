d8ac27a02a55e3d1c575ab8b7ca2546dfeeb637d
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-07-15 16:08:56 +0900

    fix: 공통 Box Floating Round 버튼 컴포넌트 삭제 #66

--- knock/src/core/ui/buttons/ShareButtons/LinkShareButton/LinkShareButton.tsx
+++ knock/src/core/ui/buttons/ShareButtons/LinkShareButton/LinkShareButton.tsx
@@ -1,8 +1,7 @@
 import { ReactComponent as Icon } from '../../../../assets/icon/Link-light.svg';
-import RoundButton from '../../RoundButton/RoundButton';
 import useCopyCurrentUrl from '../../../../../lib/hooks/useCopyCurrentUrl';
-import styles from './LinkShareButton.module.scss';
 import UButton from '../../UButton/UButton';
+import styles from './LinkShareButton.module.scss';
 
 const LinkShareButton = () => {
   const [isCopied, copyCurrentUrl] = useCopyCurrentUrl();
