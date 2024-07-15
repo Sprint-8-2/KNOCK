3d70a96def7851f5d25a3e1a3e32dd316c1c43ff
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-07-15 12:34:33 +0900

    fix: 공통 Box버튼컴포넌트 border 수정 #2
    - css border 속성을 inline 속성으로 수정

--- knock/src/pages/MainPage/MainPage.tsx
+++ knock/src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,13 @@
+import BoxButton from '../../core/ui/buttons/BoxButton/BoxButton';
+
 function MainPage() {
-  return <></>;
+  return (
+    <>
+      <BoxButton handleClick={() => {}} isLightTheme={true}>
+        버튼
+      </BoxButton>
+    </>
+  );
 }
 
 export default MainPage;
