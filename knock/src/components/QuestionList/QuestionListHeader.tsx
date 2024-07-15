34fbd049403af595c66300e22afb24d0c5b7fc67
L1m3Kun <sungku2757@gmail.com>
2024-07-15 19:44:19 +0900

    feat: 질문 목록 페이지 상단 navgate 연결 #51

--- knock/src/components/QuestionList/QuestionListHeader.tsx
+++ knock/src/components/QuestionList/QuestionListHeader.tsx
@@ -6,26 +6,30 @@ import ArrowRightIcon from '../../core/assets/icon/Hyphen-Arrow-right.svg';
 import styles from './QuestionListHeader.module.scss';
 import Icon from '../../core/ui/CommonIcon/icon';
 import Image from '../../core/ui/CommonImage/Image';
+import { Link } from 'react-router-dom';
 
 const QuestionListHeader = () => {
   return (
     <section className={styles['question-list-header']}>
-      <Image
-        imageClassName={styles['question-list-header__img']}
-        src={LogoImage}
-        alt="메인 로고"
-      />
-
-      <BoxButton handleClick={() => {}} isLightTheme={true}>
-        <p className={styles['question-list-header__button-content']}>
-          <span>답변하러 가기</span>
-          <Icon
-            className={styles['question-list-header__button-icon']}
-            src={ArrowRightIcon}
-            alt="오른쪽 화살표"
-          />
-        </p>
-      </BoxButton>
+      <Link to="/">
+        <Image
+          imageClassName={styles['question-list-header__img']}
+          src={LogoImage}
+          alt="메인 로고"
+        />
+      </Link>
+      <Link to="/post">
+        <BoxButton handleClick={() => {}} isLightTheme={true}>
+          <p className={styles['question-list-header__button-content']}>
+            <span>답변하러 가기</span>
+            <Icon
+              className={styles['question-list-header__button-icon']}
+              src={ArrowRightIcon}
+              alt="오른쪽 화살표"
+            />
+          </p>
+        </BoxButton>
+      </Link>
     </section>
   );
 };
