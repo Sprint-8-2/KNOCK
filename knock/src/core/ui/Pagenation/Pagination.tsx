75d913805ef420f5d510b22cb862b6a847a8d1a6
L1m3Kun <sungku2757@gmail.com>
2024-07-16 17:59:16 +0900

    feat: 공통 pagination 다음/이전 버튼 기능 추가 #41

--- knock/src/core/ui/Pagenation/Pagination.tsx
+++ knock/src/core/ui/Pagenation/Pagination.tsx
@@ -1,42 +1,60 @@
 import PaginationButton from './PaginationButton';
 
 import styles from '../../styles/pagination.module.scss';
+import { useState } from 'react';
 
 interface PaginationProps {
   pageIndexes: number[];
   currentPage: number;
-  handleNextIndexes: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
-  handlePrevIndexes: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
-  handleCurrentPage: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+  handleCurrentPage: (newIndex: number) => void;
 }
 
 const Pagination = ({
   pageIndexes,
   currentPage,
-  handleNextIndexes,
-  handlePrevIndexes,
   handleCurrentPage,
 }: PaginationProps) => {
+  const [currentPageIndexes, setCurrentPageIndexes] = useState<number[]>(
+    pageIndexes.slice(0, 5),
+  );
+  const [prevDisabled, setPrevDisabled] = useState<boolean>(true);
+  const [nextDisabled, setNextDisabled] = useState<boolean>(() =>
+    pageIndexes.length > 5 ? false : true,
+  );
+
   const handleListClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     const target = e.target as HTMLElement;
+    let newIndex = currentPage;
     switch (target.innerText) {
       case '<':
-        handlePrevIndexes(e);
+        newIndex = (Math.floor(currentPage / 5) - 1) * 5 + 1;
+        setCurrentPageIndexes([
+          ...pageIndexes.slice(newIndex - 1, newIndex + 4),
+        ]);
+        setNextDisabled(false);
+        setPrevDisabled(newIndex <= 5 ? true : false);
         break;
       case '>':
-        handleNextIndexes(e);
+        newIndex = Math.ceil(currentPage / 5) * 5 + 1;
+        setCurrentPageIndexes([
+          ...pageIndexes.slice(newIndex - 1, newIndex + 4),
+        ]);
+        setNextDisabled(newIndex + 5 > pageIndexes.length ? true : false);
+        setPrevDisabled(false);
         break;
       default:
-        handleCurrentPage(e);
+        newIndex = Number(target.innerText);
         break;
     }
+    handleCurrentPage(newIndex);
   };
+
   return (
     <ul className={styles['pagination']} onClick={handleListClick}>
       <li className={styles['pagination__list']}>
-        <PaginationButton>{`<`}</PaginationButton>
+        <PaginationButton isDisabled={prevDisabled}>{`<`}</PaginationButton>
       </li>
-      {pageIndexes.map((e) => {
+      {currentPageIndexes.map((e) => {
         return (
           <li key={e}>
             <PaginationButton isSelected={currentPage === e}>
@@ -47,7 +65,7 @@ const Pagination = ({
       })}
 
       <li className={styles['pagination__list']}>
-        <PaginationButton>{`>`}</PaginationButton>
+        <PaginationButton isDisabled={nextDisabled}>{`>`}</PaginationButton>
       </li>
     </ul>
   );
--- knock/src/core/ui/Pagenation/PaginationButton.tsx
+++ knock/src/core/ui/Pagenation/PaginationButton.tsx
@@ -3,18 +3,21 @@ import styles from '../../styles/pagination.module.scss';
 interface PaginationButtonProps {
   children: React.ReactNode;
   isSelected?: boolean;
+  isDisabled?: boolean;
   onClick?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
 
 const PaginationButton = ({
   children,
   onClick = () => {},
+  isDisabled = false,
   isSelected = false,
 }: PaginationButtonProps) => {
   return (
     <button
       className={`${styles['pagination__button']} ${isSelected ? styles['pagination__button--selected'] : ''}`}
       onClick={onClick}
+      disabled={isDisabled}
     >
       {children}
     </button>
