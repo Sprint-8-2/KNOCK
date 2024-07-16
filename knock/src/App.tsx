2ce5220534a018d9dd79df0d8dc2e5ad1bbe06fc
DESKTOP-LJCO8JL\장용한 <1@gmail.com>
2024-07-16 19:57:11 +0900

    feature : modal컴포넌트 api 추가

--- knock/src/App.tsx
+++ knock/src/App.tsx
@@ -1,9 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
+import ModalPage from './pages/ModalPage';
+import prifileimage from './core/assets/image/Profilephoto.svg';
 
 function App() {
   return (
     <BrowserRouter>
+      <ModalPage
+        name="아초는 고양이"
+        alt="고양이귀여워"
+        src={prifileimage}
+        questionId="1"
+      />
       <Routes>
         <Route index element={<MainPage />} />
       </Routes>
