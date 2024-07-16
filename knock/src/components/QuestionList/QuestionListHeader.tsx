f59ef44f3a5bc8b888a60802469cd87ee71f73a7
L1m3Kun <sungku2757@gmail.com>
2024-07-16 18:24:27 +0900

    fix: 질문 목록 페이지 답변하러가기 버튼 userInfo 수정 #51

--- knock/src/components/QuestionList/QuestionListHeader.tsx
+++ knock/src/components/QuestionList/QuestionListHeader.tsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
 import UButton from '../../core/ui/buttons/UButton/UButton';
-
-import LogoImage from '../../core/assets/image/SubPageLogo.svg';
-import ArrowRightIcon from '../../core/assets/icon/Hyphen-Arrow-right.svg';
-
-import styles from './QuestionListHeader.module.scss';
 import Icon from '../../core/ui/CommonIcon/icon';
 import Image from '../../core/ui/CommonImage/Image';
 
+import styles from './QuestionListHeader.module.scss';
+import LogoImage from '../../core/assets/image/SubPageLogo.svg';
+import ArrowRightIcon from '../../core/assets/icon/Hyphen-Arrow-right.svg';
+
+interface UserInfo {
+  id: number;
+  name: string;
+  imageSource: string;
+}
+
 const QuestionListHeader = () => {
-  const userId = window.localStorage.getItem('userId');
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  useEffect(() => {
+    const userInfoStr = window.localStorage.getItem('userInfo');
+    if (userInfoStr) {
+      setUserInfo(JSON.parse(userInfoStr));
+    }
+  }, []);
+
   return (
     <section className={styles['question-list-header']}>
       <Link to="/">
@@ -19,7 +34,7 @@ const QuestionListHeader = () => {
           alt="메인 로고"
         />
       </Link>
-      <Link to={userId ? `/post/${userId}/answer` : '/'}>
+      <Link to={userInfo ? `/post/${userInfo.id}/answer` : '/'}>
         <UButton type="box" handleClick={() => {}} isLightTheme={true}>
           <p className={styles['question-list-header__button-content']}>
             <span>답변하러 가기</span>
