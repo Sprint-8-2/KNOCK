86ebd748990c7d3890d82a56be6a2819bcc344ab
L1m3Kun <sungku2757@gmail.com>
2024-07-16 18:16:41 +0900

    feat: 질문 목록 페이지 pagination API 연결 #52

--- knock/src/components/QuestionList/QuestionCardList.tsx
+++ knock/src/components/QuestionList/QuestionCardList.tsx
@@ -1,53 +1,19 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { SubjectDetailResponse } from '../../core/types/api/Response';
-import { SubjectListParams } from '../../core/types/api/Request';
-import { getSubjectList } from '../../lib/api/Subject';
 import QuestionCard from './QuestionCard';
 
 import styles from './QuestionCardList.module.scss';
 
 interface QuestionCardListProps {
-  order: string;
-  indexLimit: number;
-  currentPage: number;
-  setIndexLimit: (newVal: number) => void;
-  setCurrentPage: (newVal: number) => void;
-  setMaxIndex: (newVal: number) => void;
+  questions: SubjectDetailResponse[];
 }
 
-const QuestionCardList = ({
-  order,
-  indexLimit,
-  currentPage,
-  setIndexLimit,
-  setCurrentPage,
-  setMaxIndex,
-}: QuestionCardListProps) => {
-  const [questions, setQuestions] = useState<SubjectDetailResponse[]>([]);
-  const [ulClassName, setUlClassName] = useState<string>(
-    styles['question-list-main__cards--large'],
-  );
-
-  const handleQuestions = async ({
-    limit,
-    offset,
-    sort,
-  }: SubjectListParams) => {
-    const { count, results } = await getSubjectList({ limit, offset, sort });
-    setQuestions(results);
-    setCurrentPage(1);
-    setMaxIndex(count);
-  };
-
-  useEffect(() => {
-    const sort = order === '이름순' ? 'name' : 'time';
-    handleQuestions({ limit: indexLimit, offset: currentPage, sort });
-  }, [order]);
-
+const QuestionCardList = ({ questions }: QuestionCardListProps) => {
   return (
-    <ul className={`${styles['question-list-main__cards']} ${ulClassName}`}>
+    <ul
+      className={`${styles['question-list-main__cards']} ${styles['question-list-main__cards--large']}`}
+    >
       {questions.map((e) => {
         return (
           <li key={e.id}>
--- knock/src/components/QuestionList/QuestionListPagination.tsx
+++ knock/src/components/QuestionList/QuestionListPagination.tsx
@@ -1,50 +1,61 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Pagination from '../../core/ui/Pagenation/Pagination';
 import QuestionCardList from './QuestionCardList';
 
 import styles from './QuestionListPagination.module.scss';
+import { SubjectDetailResponse } from '../../core/types/api/Response';
+import { SubjectListParams } from '../../core/types/api/Request';
+import { getSubjectList } from '../../lib/api/Subject';
 
 interface QuestionListPaginationProps {
   order: string;
 }
 
 const QuestionListPagination = ({ order }: QuestionListPaginationProps) => {
-  const [pageIndexes, setPageIndexes] = useState<number[]>([1, 2]);
+  const [questions, setQuestions] = useState<SubjectDetailResponse[]>([]);
+  const [pageIndexes, setPageIndexes] = useState<number[]>([1]);
   const [currentIndex, setCurrentIndex] = useState<number>(1);
-  const [maxIndex, setMaxIndex] = useState<number>(1);
-  const [indexLimit, setIndexLimit] = useState<number>(8);
+  const [pageQuestionCount, setPageQuestionCount] = useState<number>(8);
+  const [questionAllCounts, setQuestionAllCounts] = useState<number>(40);
 
-  const handlecurrentIndex = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    setCurrentIndex(Number((e.target as HTMLElement).innerText));
+  const handlecurrentIndex = (newValue: number) => {
+    setCurrentIndex(newValue);
   };
 
-  const handlePrevIndexes = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+  useEffect(() => {
     setCurrentIndex(1);
-  };
-  const handleNextIndexes = () => {};
+  }, [order]);
 
-  // useEffect(() => {
-  //   if (indexLimit >= maxIndex) {
-  //   } else if (indexLimit < maxIndex) {
-  //   }
-  //   return () => {};
-  // }, [maxIndex, indexLimit]);
+  useEffect(() => {
+    const maxIndex = Math.ceil(questionAllCounts / pageQuestionCount);
+    setPageIndexes(
+      Array(maxIndex)
+        .fill(0)
+        .map((_, i) => i + 1),
+    );
+  }, [questionAllCounts, pageQuestionCount]);
+
+  const handleQuestions = useCallback(
+    async ({ limit, offset, sort }: SubjectListParams) => {
+      const { count, results } = await getSubjectList({ limit, offset, sort });
+      setQuestions(results);
+      setQuestionAllCounts(count);
+    },
+    [order, currentIndex],
+  );
+
+  useEffect(() => {
+    const sort = order === '이름순' ? 'name' : 'time';
+    handleQuestions({ limit: pageQuestionCount, offset: currentIndex, sort });
+  }, [handleQuestions]);
+
   return (
     <section className={styles['question-list-main__pagination']}>
-      <QuestionCardList
-        order={order}
-        indexLimit={indexLimit}
-        currentPage={currentIndex}
-        setIndexLimit={setIndexLimit}
-        setCurrentPage={setCurrentIndex}
-        setMaxIndex={setMaxIndex}
-      />
+      <QuestionCardList questions={questions} />
       <Pagination
-        handleCurrentPage={handlecurrentIndex}
         currentPage={currentIndex}
-        handlePrevIndexes={handlePrevIndexes}
-        handleNextIndexes={handleNextIndexes}
         pageIndexes={pageIndexes}
+        handleCurrentPage={handlecurrentIndex}
       />
     </section>
   );
