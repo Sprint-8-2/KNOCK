5f2d9178add997fa9f3f506569494a43f1c2368b
L1m3Kun <sungku2757@gmail.com>
2024-07-18 18:16:02 +0900

    feat: 피드 프로필 공유하기 기능 추가 #101

--- knock/src/components/Profile/Profile.tsx
+++ knock/src/components/Profile/Profile.tsx
@@ -5,20 +5,29 @@ import defaultImage from '../../core/assets/image/Profilephoto.svg';
 import Image from '../../core/ui/CommonImage/Image';
 
 interface ProfileProps {
+  profileWrapClassName?: string;
   profileImage: string;
   name: string;
+  copySuccess: () => void;
+  copyError: () => void;
 }
 
-const Profile = ({ profileImage, name }: ProfileProps) => {
+const Profile = ({
+  profileWrapClassName,
+  profileImage,
+  name,
+  copySuccess,
+  copyError,
+}: ProfileProps) => {
   return (
-    <div className={`${styles['feed-profile']}`}>
+    <div className={profileWrapClassName ?? `${styles['feed-profile']}`}>
       <Image
         src={profileImage || defaultImage}
         alt="피드 프로필"
         imageClassName={styles['feed-profile--img']}
       />
       <div className={`${styles['feed-profile__name']}`}>{name}</div>
-      <ShareButtons />
+      <ShareButtons copySuccess={copySuccess} copyError={copyError} />
     </div>
   );
 };
