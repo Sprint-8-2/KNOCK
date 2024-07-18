2a938f2ffe8e083c01b3d859bf0fba0cf2f32d3d
seunghyun <tmdgus7048@naver.com>
2024-07-18 16:40:50 +0900

    fix: 답변컴포넌트 answerProfile컴포넌트 추가 #65

--- knock/src/components/feed/Answer/Answer.tsx
+++ knock/src/components/feed/Answer/Answer.tsx
@@ -1,10 +1,9 @@
-import Image from '../../../core/ui/CommonImage/Image';
 import AnswerForm from '../AnswerForm/AnswerForm';
 import styles from './Answer.module.scss';
 import AnswerContent from '../AnswerContent/AnswerContent';
 import AnswerRejection from '../AnswerRejection/AnswerRejection';
 import AnswerModification from '../AnswerModification/AnswerModification';
-import useElapsedTime from '../../../lib/hooks/useElapsedTime';
+import AnswerProfile from '../AnswerProfile/AnswerProfile';
 
 export type AnswerState = 'answered' | 'empty' | 'rejected';
 
@@ -39,48 +38,36 @@ const Answer = ({
   answerModificationSubmit = () => {},
   answerSubmit = () => {},
 }: AnswerProps) => {
-  const { elapsedTime } = useElapsedTime();
   const isShowProfile = answerState !== 'empty' || mode === 'answer';
 
   return (
     <div className={styles['answer']}>
       {isShowProfile && (
-        <Image
-          src={imageSource}
-          alt="profileImg"
-          containerClassName={styles['answer__profile']}
-          imageClassName={styles['answer__profile-image']}
+        <AnswerProfile
+          name={name}
+          imageSource={imageSource}
+          createAt={createAt}
         />
       )}
-      <div className={styles['answer__details']}>
-        {isShowProfile && (
-          <div className={styles['answer__row']}>
-            <h1 className={styles['answer__username']}>{name}</h1>
-            <p className={styles['answer__timestamp']}>
-              {elapsedTime(createAt)}
-            </p>
-          </div>
-        )}
-        {isModification ? (
-          <AnswerModification
-            handleSubmit={answerModificationSubmit}
-            content={content}
-            answerId={answerId}
-          />
-        ) : (
-          <>
-            {answerState === 'answered' && <AnswerContent content={content} />}
-            {answerState === 'empty' && mode === 'answer' && (
-              <AnswerForm
-                content={content}
-                questionId={questionId}
-                handleSubmit={answerSubmit}
-              />
-            )}
-            {answerState === 'rejected' && <AnswerRejection />}
-          </>
-        )}
-      </div>
+      {isModification ? (
+        <AnswerModification
+          handleSubmit={answerModificationSubmit}
+          content={content}
+          answerId={answerId}
+        />
+      ) : (
+        <>
+          {answerState === 'answered' && <AnswerContent content={content} />}
+          {answerState === 'empty' && mode === 'answer' && (
+            <AnswerForm
+              content={content}
+              questionId={questionId}
+              handleSubmit={answerSubmit}
+            />
+          )}
+          {answerState === 'rejected' && <AnswerRejection />}
+        </>
+      )}
     </div>
   );
 };
