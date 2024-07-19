81e8bd27bf4f458c0651aa2099247e8337823fd0
OK <ok@OK-2.local>
2024-07-19 21:20:21 +0900

    Feature:MainPage추가#91

--- knock/src/components/MainPageInput/MainPageInput.tsx
+++ knock/src/components/MainPageInput/MainPageInput.tsx
@@ -3,41 +3,21 @@ import styles from './mainPageInput.module.scss';
 import React, { useState } from 'react';
 import UButton from '../../core/ui/buttons/UButton/UButton';
 import Input from '../../core/ui/Input/Input';
-import { createSubject } from '../../lib/api/Subject';
-import { useNavigate } from 'react-router-dom';
-import useUserInfo from '../../lib/hooks/useUserInfo';
 import Icon from '../../core/ui/CommonIcon/icon';
 
-interface MainPageInputProps {}
+interface MainPageInputProps {
+  name: string;
+  handleNameSet: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSubmit: (e: React.FormEvent<HTMLElement>) => void;
+  handleKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+}
 
-const MainPageInput: React.FC<MainPageInputProps> = () => {
-  const [name, setName] = useState('');
-  const navigate = useNavigate();
-  const { handleUserInfo } = useUserInfo();
-
-  const handleNameSet = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
-    e.preventDefault();
-    try {
-      const { id, name: subName, imageSource } = await createSubject({ name });
-      navigate(`/post/${id}/answer`);
-      handleUserInfo({ id, name: subName, imageSource });
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (name && e.key === 'Enter') {
-      handleSubmit(e);
-    }
-  };
-
-  function handleClick(e: React.MouseEvent<HTMLElement, MouseEvent>): void {}
-
+const MainPageInput = ({
+  name,
+  handleNameSet,
+  handleSubmit,
+  handleKeyDown,
+}: MainPageInputProps) => {
   return (
     <div className={`${styles['container']}`}>
       <form className={`${styles['container__outside']}`}>
@@ -46,7 +26,7 @@ const MainPageInput: React.FC<MainPageInputProps> = () => {
             src={PersonIcon}
             alt="Person"
             className={`${styles['container__img']}`}
-            handleClick={handleClick}
+            handleClick={() => {}}
           />
         </Input>
         <UButton
--- knock/src/pages/MainPage.tsx
+++ knock/src/pages/MainPage.tsx
@@ -1,11 +1,78 @@
+import MainPageInput from '../components/MainPageInput/MainPageInput';
+import Image from '../core/ui/CommonImage/Image';
 import MetaTags from '../core/ui/MetaTags/MetaTags';
+import MainPageLogo from '../core/assets/image/MainPgaeLogo.svg';
+import backgroundImg from '../core/assets/image/mainimage.png';
+import rightArrow from '../core/assets/icon/Hyphen-Arrow-right.svg';
+import styles from './MainPage.module.scss';
+import UButton from '../core/ui/buttons/UButton/UButton';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createSubject } from '../lib/api/Subject';
+import useUserInfo from '../lib/hooks/useUserInfo';
 
-function MainPage() {
+const MainPage = () => {
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+  const { handleUserInfo } = useUserInfo();
+
+  const handleNameSet = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (name && e.key === 'Enter') {
+      handleSubmit(e);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    try {
+      const { id, name: subName, imageSource } = await createSubject({ name });
+      navigate(`/post/${id}/answer`);
+      handleUserInfo({ id, name: subName, imageSource });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
-    <>
+    <div className={`${styles['mainPage']}`}>
       <MetaTags />
-    </>
+      <UButton
+        type="box"
+        handleClick={handleSubmit}
+        isLightTheme={true}
+        className={`${styles['mainPage__answer']}`}
+      >
+        답변하러 가기
+        <Image
+          src={rightArrow}
+          alt="rightArrow"
+          imageClassName={styles['mainPage__answer__rightArrowImg']}
+        />
+      </UButton>
+      <Image
+        src={MainPageLogo}
+        alt="메인페이지로고"
+        imageClassName={styles['mainPage__logo']}
+      />
+      <div className={`${styles['mainPage__mainInput']}`}>
+        <MainPageInput
+          name={name}
+          handleNameSet={handleNameSet}
+          handleSubmit={handleSubmit}
+          handleKeyDown={handleKeyDown}
+        />
+      </div>
+      <Image
+        src={backgroundImg}
+        alt="배경이미지"
+        imageClassName={styles['mainPage__bg-img']}
+      />
+    </div>
   );
-}
+};
 
 export default MainPage;
