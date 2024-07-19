8353c2e9474ca30743af85fc8fd22a45ef68a2d6
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-07-19 14:05:03 +0900

    feat: localStorage util 추가 #109

---
+++ knock/src/lib/util/localStorageUtil.ts
@@ -0,0 +1,36 @@
+/**
+ * JSON.parse 가능 여부 체크용 함수
+ * @param jsonString
+ * @returns boolean JSON.parse 가능 여부
+ */
+const isValidJSON = (jsonString: string) => {
+  try {
+    JSON.parse(jsonString);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const localStorageUtil = {
+  set: (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get(key: string) {
+    const result = localStorage.getItem(key);
+    if (result === null) {
+      return null;
+    }
+
+    if (!isValidJSON(result)) {
+      throw new Error('can not parse json error');
+    }
+
+    return JSON.parse(result);
+  },
+  remove: (key: string) => {
+    localStorage.removeItem(key);
+  },
+};
+
+export default localStorageUtil;
