e404fe49904c3b420ea6628ac6e9b39a78ae7da4
wonsic3686@gmail.com <wonsic3686@gmail.com>
2024-07-21 06:43:35 +0900

    feat: react query 세팅 추가 #110
    - @tanstack/react-query 추가
    
    - react-infinite-scroller 추가

--- knock/src/App.tsx
+++ knock/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import AnswerPage from './pages/AnswerPage';
 import MainPage from './pages/MainPage';
@@ -7,18 +8,21 @@ import QuestionList from './pages/QuestionList';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<MainPage />} />
-        <Route path="list" element={<QuestionList />} />
-        <Route path="post/:id">
-          <Route index element={<PostPage />} />
-          <Route path="answer" element={<AnswerPage />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path="list" element={<QuestionList />} />
+          <Route path="post/:id">
+            <Route index element={<PostPage />} />
+            <Route path="answer" element={<AnswerPage />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
