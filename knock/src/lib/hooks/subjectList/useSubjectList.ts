2ea1d5c760fc95dfaf5c4371e2ccbb37be21cec6
L1m3Kun <sungku2757@gmail.com>
2024-07-21 23:44:15 +0900

    fix: 질문 목록 페이지 페이지 너비에 따라 요소 개수 안 바뀌는 오류 수정 #127

--- knock/src/lib/hooks/subjectList/useSubjectList.ts
+++ knock/src/lib/hooks/subjectList/useSubjectList.ts
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import useResize from '../useResize';
 import { SubjectDetailResponse } from '../../../core/types/api/Response';
@@ -9,6 +9,13 @@ interface SubjectListFuncParams {
 }
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
+const DEFAULT_SUBJECT = {
+  name: '',
+  imageSource: '',
+  questionCount: 0,
+  createdAt: '',
+};
+
 const useSubjectList = ({ order }: SubjectListFuncParams) => {
   const { pageSize } = useResize();
   const [subjects, setSubjects] = useState<SubjectDetailResponse[]>([]);
@@ -16,42 +23,79 @@ const useSubjectList = ({ order }: SubjectListFuncParams) => {
   const [currentIndex, setCurrentIndex] = useState<number>(1);
   const [isLoad, setIsLoad] = useState<boolean>(false);
 
-  const { data, isLoading } = useQuery({
+  const handleQueryRequest = async (page: number) => {
+    const query = () => {
+      setIsLoad(true);
+      const queryArray = [];
+      if (page) {
+        queryArray.push(`limit=${page}`);
+        queryArray.push(`offset=${page * (currentIndex - 1)}`);
+      }
+
+      if (order) {
+        queryArray.push(`sort=${order}`);
+      }
+      return queryArray.join('&');
+    };
+
+    const path = `subjects/?${query()}/`;
+
+    return await fetch(`${BASE_URL}${path}`).then((res) => res.json());
+  };
+
+  const { data, isError, isSuccess, isPending } = useQuery({
     queryKey: ['subjectList', currentIndex, order, pageSize],
-    queryFn: async () => {
-      const query = () => {
-        setIsLoad(true);
-        const queryArray = [];
-        if (pageSize) {
-          queryArray.push(`limit=${pageSize}`);
-          queryArray.push(`offset=${pageSize * (currentIndex - 1)}`);
-        }
-
-        if (order) {
-          queryArray.push(`sort=${order}`);
-        }
-        return queryArray.join('&');
-      };
-
-      const path = `subjects/?${query()}`;
-
-      return await fetch(`${BASE_URL}${path}/`).then((res) => res.json());
-    },
+    queryFn: () => handleQueryRequest(pageSize),
+    refetchOnWindowFocus: false,
   });
+
+  // const queryClient = useQueryClient();
+
   const handleCurrentIndex = (newValue: number) => {
     setCurrentIndex(newValue);
   };
 
+  // useEffect(() => {
+  //   if (pageSize === 6) {
+  //     queryClient.prefetchQuery({
+  //       queryKey: ['subjectListPre'],
+  //       queryFn: () => handleQueryRequest(8),
+  //     });
+  //   } else if (pageSize === 8) {
+  //     queryClient.prefetchQuery({
+  //       queryKey: ['subjectListPre'],
+  //       queryFn: () => handleQueryRequest(6),
+  //     });
+  //   }
+  // }, []);
+
   useEffect(() => {
-    if (!isLoading) {
-      setTimeout(() => {
-        setIsLoad(false);
-      }, 1000);
-      const { count, results } = data;
-      setSubjects(results);
-      setMaxIndex(Math.ceil(count / pageSize));
+    if (isPending) {
+      setSubjects(
+        Array(pageSize)
+          .fill(0)
+          .map((e, i) => ({
+            id: e + i,
+            ...DEFAULT_SUBJECT,
+          })),
+      );
+    } else {
+      if (isSuccess) {
+        if (subjects.length === 0) {
+          setTimeout(() => {
+            setIsLoad(false);
+          }, 1000);
+        } else {
+          setIsLoad(false);
+        }
+        const { count, results } = data;
+        setSubjects(results);
+        setMaxIndex(Math.ceil(count / pageSize));
+      } else if (isError) {
+        console.log('api error');
+      }
     }
-  }, [data, isLoading]);
+  }, [data, isError, isSuccess, isPending]);
 
   useEffect(() => {
     handleCurrentIndex(1);
