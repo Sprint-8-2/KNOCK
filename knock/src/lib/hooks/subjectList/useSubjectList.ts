79f30185f46e399274b6fdca526ca1d21806c01e
L1m3Kun <sungku2757@gmail.com>
2024-07-22 10:42:20 +0900

    fix: 질문 목록 페이지 prefetching 적용 #127

--- knock/src/lib/hooks/subjectList/useSubjectList.ts
+++ knock/src/lib/hooks/subjectList/useSubjectList.ts
@@ -3,6 +3,7 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import useResize from '../useResize';
 import { SubjectDetailResponse } from '../../../core/types/api/Response';
+import { SubjectGetDetailParams } from '../../../core/types/api/Request';
 
 interface SubjectListFuncParams {
   order: 'name' | 'time';
@@ -18,10 +19,26 @@ const DEFAULT_SUBJECT = {
 
 const useSubjectList = ({ order }: SubjectListFuncParams) => {
   const { pageSize } = useResize();
-  const [subjects, setSubjects] = useState<SubjectDetailResponse[]>([]);
-  const [maxIndex, setMaxIndex] = useState<number>(0);
+  const [subjects8, setSubjects8] = useState<SubjectDetailResponse[]>(
+    Array(8)
+      .fill(0)
+      .map((e, i) => ({
+        id: e + i,
+        ...DEFAULT_SUBJECT,
+      })),
+  );
+  const [subjects6, setSubjects6] = useState<SubjectDetailResponse[]>(
+    Array(6)
+      .fill(0)
+      .map((e, i) => ({
+        id: e + i,
+        ...DEFAULT_SUBJECT,
+      })),
+  );
+
   const [currentIndex, setCurrentIndex] = useState<number>(1);
   const [isLoad, setIsLoad] = useState<boolean>(false);
+  const [itemCount, setItemCount] = useState<number>(0);
 
   const handleQueryRequest = async (page: number) => {
     const query = () => {
@@ -43,59 +60,50 @@ const useSubjectList = ({ order }: SubjectListFuncParams) => {
     return await fetch(`${BASE_URL}${path}`).then((res) => res.json());
   };
 
-  const { data, isError, isSuccess, isPending } = useQuery({
-    queryKey: ['subjectList', currentIndex, order, pageSize],
+  const { data, isError, isSuccess } = useQuery({
+    queryKey: ['subjectList', currentIndex, order],
     queryFn: () => handleQueryRequest(pageSize),
     refetchOnWindowFocus: false,
   });
 
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
   const handleCurrentIndex = (newValue: number) => {
     setCurrentIndex(newValue);
   };
 
-  // useEffect(() => {
-  //   if (pageSize === 6) {
-  //     queryClient.prefetchQuery({
-  //       queryKey: ['subjectListPre'],
-  //       queryFn: () => handleQueryRequest(8),
-  //     });
-  //   } else if (pageSize === 8) {
-  //     queryClient.prefetchQuery({
-  //       queryKey: ['subjectListPre'],
-  //       queryFn: () => handleQueryRequest(6),
-  //     });
-  //   }
-  // }, []);
+  useEffect(() => {
+    queryClient.prefetchQuery({
+      queryKey: ['subjectListPre', order, currentIndex],
+      queryFn: () =>
+        handleQueryRequest(pageSize === 6 ? 8 : 6).then((res) => {
+          if (pageSize === 6) {
+            setSubjects8(res.results);
+          } else if (pageSize === 8) {
+            setSubjects6(res.results);
+          }
+          return res;
+        }),
+    });
+  }, [order, currentIndex]);
 
   useEffect(() => {
-    if (isPending) {
-      setSubjects(
-        Array(pageSize)
-          .fill(0)
-          .map((e, i) => ({
-            id: e + i,
-            ...DEFAULT_SUBJECT,
-          })),
-      );
-    } else {
-      if (isSuccess) {
-        if (subjects.length === 0) {
-          setTimeout(() => {
-            setIsLoad(false);
-          }, 1000);
-        } else {
-          setIsLoad(false);
-        }
-        const { count, results } = data;
-        setSubjects(results);
-        setMaxIndex(Math.ceil(count / pageSize));
-      } else if (isError) {
-        console.log('api error');
+    if (isSuccess) {
+      setTimeout(() => {
+        setIsLoad(false);
+      }, 1000);
+
+      const { count, results } = data;
+      setItemCount(count);
+      if (pageSize === 8) {
+        setSubjects8(results);
+      } else if (pageSize === 6) {
+        setSubjects6(results);
       }
+    } else if (isError) {
+      console.log('api error');
     }
-  }, [data, isError, isSuccess, isPending]);
+  }, [data, isError, isSuccess]);
 
   useEffect(() => {
     handleCurrentIndex(1);
@@ -104,8 +112,8 @@ const useSubjectList = ({ order }: SubjectListFuncParams) => {
   return {
     isLoading: isLoad,
     currentIndex,
-    maxIndex,
-    subjects,
+    itemCount,
+    subjects: pageSize === 6 ? subjects6 : subjects8,
     handleCurrentIndex,
   };
 };
--- knock/src/pages/SubjectList.tsx
+++ knock/src/pages/SubjectList.tsx
@@ -1,11 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import SubjectListHeader from '../components/SubjectList/SubjectListHeader';
 import SubjectListMain from '../components/SubjectList/SubjectListMain';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 const SubjectList = () => {
   return (
     <>
-      <SubjectListHeader />
-      <SubjectListMain />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <SubjectListHeader />
+        <SubjectListMain />
+      </QueryClientProvider>
     </>
   );
 };
